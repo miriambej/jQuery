3683d3968528a21a2c2e17f7f5ca3716ab6495e5
miriambej <justme0112@hotmail.com>
2017-11-19 18:32:05 +1100

    custom animations

--- base/js/script.js
+++ base/js/script.js
@@ -27,14 +27,25 @@ $(function() {
   // $("p").hide(); 
   // $("p").slideDown(1000); 
 
-  $(".blue-box").animate({
-    "margin-left":"+=200px"
-  }, 2000, "linear"); 
+  // $(".blue-box").animate({
+  //   "margin-left":"+=200px"
+  // }, 2000, "linear"); 
+
+  // $(".blue-box").animate({
+  //   "margin-left":"-=200px"
+  // }, 2000, "linear");
 
   $(".blue-box").animate({
-    "margin-left":"-=200px"
-  }, 2000, "linear");
+    "margin-left":"200px",
+    "opacity":"0",
+    "height":"50px",
+    "width":"50px",
+    "margin-top": "25px"
+  }, 1000);
 
+$("p").animate({
+    "font-size":"20px"
+  }, 1000);
 
 
  
