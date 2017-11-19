84261394f50a10c8f8a9e96f826fbe0462dcebd0
miriambej <justme0112@hotmail.com>
2017-11-19 19:01:46 +1100

    delaying and chaining animations

--- base/js/script.js
+++ base/js/script.js
@@ -35,18 +35,22 @@ $(function() {
   //   "margin-left":"-=200px"
   // }, 2000, "linear");
 
-  $(".blue-box").animate({
-    "margin-left":"200px",
-    "opacity":"0",
-    "height":"50px",
-    "width":"50px",
-    "margin-top": "25px"
-  }, 1000);
+//   $(".blue-box").animate({
+//     "margin-left":"200px",
+//     "opacity":"0",
+//     "height":"50px",
+//     "width":"50px",
+//     "margin-top": "25px"
+//   }, 1000);
 
-$("p").animate({
-    "font-size":"20px"
-  }, 1000);
+// $("p").animate({
+//     "font-size":"20px"
+//   }, 1000);
 
+//delay so the animation starts once the previous one finish
+$(".red-box").fadeTo(1000, 0.2);
+$(".green-box").delay(1000).fadeTo(1000, 0.5);
+$(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn();
 
  
 });
